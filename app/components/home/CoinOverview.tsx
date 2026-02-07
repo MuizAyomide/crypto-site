0f8d52a7a287502c0f61bad57692bf3e4a1c918d
MuizAyomide <Akinrinolamuiz22@gmail.com>
2026-02-07 04:02:43 +0000

    add error handling to coin overview component

--- app/components/home/CoinOverview.tsx
+++ app/components/home/CoinOverview.tsx
@@ -2,23 +2,29 @@ import { fetcher } from '@/lib/coingecko.action';
 import { cn, formatCurrency } from '@/lib/utils';
 import Image from 'next/image';
 import React from 'react'
+import CoinOverviewFallback from './CoinOverviewFallback';
 
 export default async function CoinOverview() {
 
-    const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
-        dex_pair_format: 'symbol'
-    });
+    let coin;
+    try {
+        const coin = await fetcher<CoinDetailsData>('/coins/bitcoin', {
+            dex_pair_format: 'symbol'
+        } catch (error) {
+            console.error('Error fetching coin details:', error);
+            return <CoinOverviewFallback />
+        });
 
 
-    return (
-        <div id='coin-overview'>
-            <div className="header">
-                <Image src={coin.image.large} width={56} height={56} alt={coin.name} />
-                <div className='info'>
-                    <p>{coin.name}/ {coin.symbol.toUpperCase()}</p>
-                    <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+        return (
+            <div id='coin-overview'>
+                <div className="header">
+                    <Image src={coin.image.large} width={56} height={56} alt={coin.name} />
+                    <div className='info'>
+                        <p>{coin.name}/ {coin.symbol.toUpperCase()}</p>
+                        <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
